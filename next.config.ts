2d21eadb6c45d54b11c01d258e81afb207bd13cc
hperezs <h.perez.saldivar@gmail.com>
2025-09-15 17:36:07 -0600

    Remove base path

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.PAGES_BASE_PATH || "",
   trailingSlash: true,
   images: {
     unoptimized: true,
